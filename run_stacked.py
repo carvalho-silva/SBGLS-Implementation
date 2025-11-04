3a93c4cae74c4196578d0fc906f44d98519f58be
Gabriela Carvalho-Silva <46339664+carvalho-silva@users.noreply.github.com>
2025-11-04 17:35:05 -0300

    Merge selected files from working branch

---
+++ run_stacked.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import LS_like_periodograms as aux
+
+df = pd.read_csv('DS1_timeSeries_b.csv')
+print(df)
+
+time = df.time
+
+RV = df.residuals
+RV_error = df.e_rv
+
+
+data = aux.stacked_periodogram(time, RV, RV_error, N_min=50,
+                               periodogram_type='GLS', p_min=1,
+                               p_max=30., num_periods = 5000,
+                               mode='random')
+                               
+aux.plot_stacked_periodogram_heatmap(data, norm='linear')
+                               
+                               
